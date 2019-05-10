482ddaf4c3600ea745a461880d3fb3c2ad60e267
T-98 <37400401+T-98@users.noreply.github.com>
2019-05-09 17:55:09 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,12 @@
-//Paste the details given to you by firebase under this comment
-//eg
-// var firebaseConfig = {
-// ...config
-// };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDRH6P79v1CwaSmHEWL0I7ZbvkYHNvvV6w",
+    authDomain: "vybhav-1255b.firebaseapp.com",
+    databaseURL: "https://vybhav-1255b.firebaseio.com",
+    projectId: "vybhav-1255b",
+    storageBucket: "vybhav-1255b.appspot.com",
+    messagingSenderId: "832630082717",
+    appId: "1:832630082717:web:cea9d903a5a3749e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
